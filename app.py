6bdbe069e4a8be9e78ed8b72f484b48707ac9d19
Magno <jmagno.dutra@hotmail.com>
2023-08-20 02:22:20 -0300

    refatorado para adicionar tarefas em apenas 1 comando

--- app.py
+++ app.py
@@ -17,51 +17,93 @@ while True:
     action = input("\nDigite adicionar, mostrar, editar, completar ou sair: ")
     action = action.strip()
 
-    match action:
-        case "adicionar":
-            todo = input("Digite uma tarefa: ") + "\n"
+    if "adicionar" in action:
+        todo = action[10:]
 
-            with open("todos.txt", "r") as file:
-                todo_list = file.readlines()
+        with open("todos.txt", "r") as file:
+            todo_list = file.readlines()
 
-            todo_list.append(todo)
+        todo_list.append(todo)
 
-            with open("todos.txt", "w") as file:
-                file.writelines(todo_list)
-        case "mostrar":
-            show_todo()
-        case "editar":
-            show_todo()
+        with open("todos.txt", "w") as file:
+            file.writelines(todo_list)
+    elif "mostrar" in action:
+        show_todo()
+    elif "editar" in action:
+        with open("todos.txt", "r") as file:
+            todo_list = file.readlines()
 
-            with open("todos.txt", "r") as file:
-                todo_list = file.readlines()
-
-            index = int(input("Digite o número da tarefa que deseja editar: "))
-            if index > len(todo_list):
-                print("Digite um número válido! Voltando ao menu")
-            else:
-                todo_list[index - 1] = input("Digite a tarefa: ") + "\n"
-
-                with open("todos.txt", "w") as file:
-                    file.writelines(todo_list)
-
-                print("Tarefa editada")
-        case "completar":
-            show_todo()
-
-            with open("todos.txt", "r") as file:
-                todo_list = file.readlines()
-
-            index = int(input("Digite o número da tarefa que deseja completar: "))
-            del todo_list[index - 1]
+        index = int(action[7:])
+        if index > len(todo_list):
+            print("Digite um número válido! Voltando ao menu")
+        else:
+            todo_list[index - 1] = input("Digite a tarefa: ") + "\n"
 
             with open("todos.txt", "w") as file:
                 file.writelines(todo_list)
 
-            print("Parabéns! Tarefa removida com sucesso.")
-        case "sair":
-            break
-        case default:
-            print("Digite um comando válido!")
+            print("Tarefa editada")
+    elif "completar" in action:
+        with open("todos.txt", "r") as file:
+            todo_list = file.readlines()
+
+        index = int(action[10:])
+        del todo_list[index - 1]
+
+        with open("todos.txt", "w") as file:
+            file.writelines(todo_list)
+
+        print("Parabéns! Tarefa removida com sucesso.")
+    elif "sair" in action:
+        break
+    else:
+        print("Digite um comando válido!")
+
+    # match action:
+    #     case "adicionar":
+    #         todo = input("Digite uma tarefa: ") + "\n"
+
+    #         with open("todos.txt", "r") as file:
+    #             todo_list = file.readlines()
+
+    #         todo_list.append(todo)
+
+    #         with open("todos.txt", "w") as file:
+    #             file.writelines(todo_list)
+    #     case "mostrar":
+    #         show_todo()
+    #     case "editar":
+    #         show_todo()
+
+    #         with open("todos.txt", "r") as file:
+    #             todo_list = file.readlines()
+
+    #         index = int(input("Digite o número da tarefa que deseja editar: "))
+    #         if index > len(todo_list):
+    #             print("Digite um número válido! Voltando ao menu")
+    #         else:
+    #             todo_list[index - 1] = input("Digite a tarefa: ") + "\n"
+
+    #             with open("todos.txt", "w") as file:
+    #                 file.writelines(todo_list)
+
+    #             print("Tarefa editada")
+    #     case "completar":
+    #         show_todo()
+
+    #         with open("todos.txt", "r") as file:
+    #             todo_list = file.readlines()
+
+    #         index = int(input("Digite o número da tarefa que deseja completar: "))
+    #         del todo_list[index - 1]
+
+    #         with open("todos.txt", "w") as file:
+    #             file.writelines(todo_list)
+
+    #         print("Parabéns! Tarefa removida com sucesso.")
+    #     case "sair":
+    #         break
+    #     case default:
+    #         print("Digite um comando válido!")
 
 print("Até logo!")

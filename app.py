b4d2aa502efa1cfc2e9f28126fc3bb406bb48847
Magno <jmagno.dutra@hotmail.com>
2023-08-22 20:22:59 -0300

    alterado checagem para starts with

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-todo_list = []
+def get_todos():
+    with open("todos.txt", "r") as file:
+        todo_list = file.readlines()
+    return todo_list
+
+
+def set_todos(todos):
+    with open("todos.txt", "w") as file:
+        file.writelines(todos)
 
 
 def show_todo():
-    with open("todos.txt", "r") as file:
-        todo_list = file.readlines()
+    todo_list = get_todos()
 
     formated_list = [todo.strip("\n") for todo in todo_list]
 
@@ -17,44 +24,40 @@ while True:
     action = input("\nDigite adicionar, mostrar, editar, completar ou sair: ")
     action = action.strip()
 
-    if "adicionar" in action:
+    if action.startswith("adicionar"):
         todo = action[10:]
 
-        with open("todos.txt", "r") as file:
-            todo_list = file.readlines()
+        todo_list = get_todos()
 
         todo_list.append(todo + "\n")
 
-        with open("todos.txt", "w") as file:
-            file.writelines(todo_list)
-    elif "mostrar" in action:
+        set_todos(todo_list)
+    elif action.startswith("mostrar"):
         show_todo()
-    elif "editar" in action:
-        with open("todos.txt", "r") as file:
-            todo_list = file.readlines()
+    elif action.startswith("editar"):
         try:
+            todo_list = get_todos()
             index = int(action[7:])
+
             if index > len(todo_list):
                 print("Digite um número válido! Voltando ao menu")
             else:
                 todo_list[index - 1] = input("Digite a tarefa: ") + "\n"
 
-                with open("todos.txt", "w") as file:
-                    file.writelines(todo_list)
+                set_todos(todo_list)
 
                 print("Tarefa editada")
         except ValueError:
             print("Digite somente o número da tarefa")
             continue
-    elif "completar" in action:
-        with open("todos.txt", "r") as file:
-            todo_list = file.readlines()
+    elif action.startswith("completar"):
         try:
+            todo_list = get_todos()
+
             index = int(action[10:])
             del todo_list[index - 1]
 
-            with open("todos.txt", "w") as file:
-                file.writelines(todo_list)
+            set_todos(todo_list)
 
             print("Parabéns! Tarefa removida com sucesso.")
         except IndexError:
@@ -63,7 +66,7 @@ while True:
         except ValueError:
             print("É necessário digitar somente o número desejado e nenhuma letra.")
             continue
-    elif "sair" in action:
+    elif action.startswith("sair"):
         break
     else:
         print("Digite um comando válido!")
